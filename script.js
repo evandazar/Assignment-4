a9df3ef7f84fab2f593ca994aafd95209b8c9c7c
Valdez <evandervaldez@my.whccd.edu>
2020-02-05 21:06:58 -0800

    finished word search function and fixed minimum fucntion

--- script.js
+++ script.js
@@ -4,18 +4,11 @@
 // console.log(min(0, -10)); // will output -10
 const min = function(min1, min2)
 {
-    if (min1 < min2)
+    if (min1 <= min2)
     {
         return min1;
     }
-    else if (min2 < min1)
-    {
-        return min2;
-    }
-    else
-    {
-        return "both numbers are equal";
-    };
+    return min2;   
 };
 console.log(min(5,10));
 console.log(min(0, -10));
@@ -23,17 +16,26 @@ console.log(min(0, -10));
 // 2. Write a function that takes the radius of a circle and outputs it’s approximate circumference (the formula for circumference is 2 *pi * radius, you may use 3.14 for pi).
 // Example Output:
 // console.log(calculateCircumference(5)); // will output 31.4
-const findcircumference = function(circlenum)
+const calculateCircumference = function(radius)
 {
-    return 2*3.14*circlenum;
+    return 2*3.14*radius;
 };
-console.log(findcircumference(5));
+console.log(calculateCircumference(5));
 
 // 3. Write a function searchString that takes a character and then returns how many times it appears in another string argument.
 // Example Output:
 // console.log(searchString(“s”, “mississippi”)); // will output 4
 // console.log(searchString(“q”, “mississippi”)); // will output 0
-const searchstring = function(search)
+const searchstring = function(search, searchlength)
 {
-    
-}
+    let snum = 0
+    for (let i = 0; i <= searchlength.length-1; i++)
+{
+    if (searchlength[i] == search)
+    {
+    snum += 1;
+    };
+};
+return snum
+};
+console.log(searchstring("s","mississippi"));
